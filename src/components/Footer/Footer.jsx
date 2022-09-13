d58089899c4dfc70fc7daedfa74a5fe7752429f3
suyash <SuyashAcharyaMITUJJAIN@gmail.com>
2022-09-13 18:22:02 +0530

    DashBoard First Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footermain">
+        <div className="FooterWrapper ">
+        <h3>Copyright © Suyash Dashbord Website 2022.</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
